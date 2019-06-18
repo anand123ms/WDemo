bca447f612913c706c72924181561a6b839c518f
anandtugaon-nihilent <anand.tugaon@nihilent.com>
2019-06-18 11:41:54 +0530

    Clean up the code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WDemo
+//Install dependency files just run the below command in terminal
+
+pod install
--- WDemo/Pods/Target Support Files/Pods-WDemo/Pods-WDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WDemo : NSObject
-@end
-@implementation PodsDummy_Pods_WDemo
-@end
